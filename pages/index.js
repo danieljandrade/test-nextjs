cd46f3c9c8ac5ca566465f45faff170e82df3106
danieljandrade <daniel_andrade@outlook.com>
2022-01-14 16:13:29 -0300

    usando o import Link

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,12 @@
-import { useState } from 'react'
-
+import Link from "next/link"
 
 function Home() {
     return (
         <div>
-            <h4>Pagina Inicial</h4>
-
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
+            <h4>Home</h4>
+            <Link href="/sobre">
+                <a>Acessar página Sobre</a>
+            </Link>
         </div>
     )
 }
